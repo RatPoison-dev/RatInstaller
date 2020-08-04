5c3d889ab11a1b712505cd0059668368834ff264
retart1337 <misha.domanov@mail.ru>
2020-08-04 22:12:44 +0300

    Make installer run ratpoison

--- installer.py
+++ installer.py
@@ -1,6 +1,9 @@
 import subprocess, string, random, requests, zipfile, os, glob, shutil, pygit2
 from clint.textui import progress
 
+installed = False
+updated = False
+
 def downloadFileWithBar(path, link):
     r = requests.get(link, stream=True)
     with open(path, 'wb') as f:
@@ -9,6 +12,10 @@ def downloadFileWithBar(path, link):
             if chunk:
                 f.write(chunk)
                 f.flush()
+
+for file in glob.glob("RatPoison.kt", recursive=True):
+    installed = True
+    break
 
 def searchDir(path):
     for file in os.listdir():
@@ -19,6 +26,7 @@ def searchDir(path):
 for file in glob.glob("version.txt"):
     # Autoupdate
     with open(file) as f:
+        updated = True
         c = f.readlines()
         origin_version = c[0].replace("\n", "")
         origin_branch = c[1].replace("\n", "")
@@ -52,15 +60,26 @@ if (not searchDir("jdk")):
     os.environ["JAVA_HOME"] = os.path.join(os.getcwd(), "jdk-14.0.2")
     os.environ["PATH"] = os.environ["PATH"]+";"+os.path.join(os.environ["JAVA_HOME"], "bin")
 
-# BUILD
-print("Building RatPoison...")
-subprocess.run(["gradlew.bat", "RatPoison"])
-if (input("Would you like to randomize the file name for safety? [Y/N]") in ["y", "yes"]):
-    random_name = getRandomName()
-    for d in os.listdir("build/"):
-        if ("RatPoison" in d):
-            for file in os.listdir(f"build/{d}"):
-                path = f"build/{d}/{file}"
-                if (os.path.isfile(path)):
-                    ext = os.path.splitext(file)[1]
-                    os.rename(path, f"build/{d}/{random_name}{ext}")
+def searchFolderName():
+    for file in os.listdir("build/"):
+        if ("RatPoison" in file):
+            return file
+
+if (not installed and not updated):
+    # BUILD
+    print("Building RatPoison...")
+    subprocess.run(["gradlew.bat", "RatPoison"])
+    if (input("Would you like to randomize the file name for safety? [Y/N] ") in ["y", "yes"]):
+        random_name = getRandomName()
+
+else:
+    java_exe = "jdk-14.0.2/bin/java.exe"
+    raw_folder_name = searchFolderName()
+    folder_name = os.path.join("build", raw_folder_name)
+    for file in os.listdir(folder_name):
+        if (".jar" in file):
+            jar_file = os.path.join(folder_name, file)
+            os.system(f"title {raw_folder_name}")
+            subprocess.run([java_exe, "-Xmx512m", "-Xms32m", "-XX:+UseSerialGC", "-jar", jar_file])
+
+os.system("pause")
