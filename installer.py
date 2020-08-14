c75b3382778d39cfcdefdc9d468ab4f5bcc2a63b
retart1337 <misha.domanov@mail.ru>
2020-08-15 01:18:42 +0300

    Improve crash handler

--- installer.py
+++ installer.py
@@ -1,11 +1,14 @@
-import traceback, os
-
+import traceback, os, utils, atexit
+def onexit():
+    utils.killJDKs()
+atexit.register(onexit)
 # Crash handler
 try:
     import main
-except:
+except BaseException:
     print("Some exception occured, please report in discord (https://discord.gg/xkTteTM):")
     print("----CUT HERE----")
     traceback.print_exc()
-    
+    utils.killJDKs()
+
 os.system("pause")
